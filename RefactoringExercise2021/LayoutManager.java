b2f6a9a399c5c99244b2d358de2bdbc9c5aa0b37
Cormac Mattimoe <cormacmattimoe98@gmail.com>
2021-03-06 19:42:22 +0000

    Added new class LayoutManager

---
+++ RefactoringExercise2021/LayoutManager.java
@@ -0,0 +1,9 @@
+
+
+public class LayoutManager {
+
+	static String loType1 = "growx, pushx";
+	static String loType2 = "growx, pushx, wrap";
+	static String loType3 = "span 2, growx, pushx, wrap";
+	
+}
